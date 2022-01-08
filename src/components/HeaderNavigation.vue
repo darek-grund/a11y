f866b19917ea868ebd88fc9d87a1e96bf7097e17
Darek Grund (PGS Software) <dgrund@pgs-soft.com>
2022-01-08 15:21:49 +0100

    Responsive navigation

--- src/components/HeaderNavigation.vue
+++ src/components/HeaderNavigation.vue
@@ -7,20 +7,25 @@
             The Insurer
             <img src="@/assets/images/logo.jpg" class="logo" alt="The Insurer Home" />
           </router-link>
-
-          <nav>
-            <ul class="nav">
+          <nav class="nav">
+            <button class="nav__toggle"
+                    :aria-expanded="isOpen"
+                    aria-label="Toggle navigation"
+                    aria-controls="main-navigation"
+                    @click="toggleNavigation"
+            ></button>
+            <ul class="nav__list" :class="{ 'nav__list--open': isOpen }" id="main-navigation">
               <li class="nav__item">
-                <router-link to="/" class="nav__link">Insurance</router-link>
+                <router-link to="/" class="nav__link" @click="closeNavigation">Insurance</router-link>
               </li>
               <li class="nav__item">
-                <router-link to="/about-us" class="nav__link">About us</router-link>
+                <router-link to="/about-us" class="nav__link" @click="closeNavigation">About us</router-link>
               </li>
               <li class="nav__item">
-                <router-link to="/claim-report" class="nav__link">Claim report</router-link>
+                <router-link to="/claim-report" class="nav__link" @click="closeNavigation">Claim report</router-link>
               </li>
               <li class="nav__item">
-                <router-link to="/contact" class="nav__link">Contact</router-link>
+                <router-link to="/contact" class="nav__link" @click="closeNavigation">Contact</router-link>
               </li>
             </ul>
           </nav>
@@ -31,8 +36,26 @@
 </template>
 
 <script>
+  import { ref } from 'vue';
+
   export default {
     name: 'HeaderNavigation',
+
+    setup() {
+      const isOpen = ref(false);
+      const toggleNavigation = () => {
+        isOpen.value = !isOpen.value;
+      };
+      const closeNavigation = () => {
+        isOpen.value = false;
+      };
+
+      return {
+        isOpen,
+        closeNavigation,
+        toggleNavigation,
+      }
+    }
   }
 </script>
 
